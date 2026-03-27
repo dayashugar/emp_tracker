b185141df2aba7492ea75b78d59072e597df6158
dayashugar <ds18hugar@gmail.com>
2026-03-27 14:31:35 +0530

    change colour to red

--- plotter.py
+++ plotter.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
 
 def plot_bar_graph_v2(x_axis, y_axis):    
-    plt.bar(x_axis, y_axis, color="orange")
+    plt.bar(x_axis, y_axis, color="red")
     plt.savefig('./data/employee_salary.png')
